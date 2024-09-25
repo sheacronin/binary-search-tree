e8d409baaf328b674e51bb4618e746bef762adfe
Shea Cronin <shealcronin@gmail.com>
2024-09-25 09:13:35 -0400

    Add driver script to show Tree

--- bin/main.rb
+++ bin/main.rb
@@ -2,12 +2,40 @@
 
 require_relative '../lib/tree'
 
-tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree = Tree.new(Array.new(15) { rand(1..100) })
 tree.pretty_print
 
-tree2 = Tree.new([1, 2, 3])
-tree2.insert(4)
-tree2.pretty_print
+puts 'balanced?'
+puts tree.balanced? # should be true
 
-tree3 = Tree.new([1, 2, 3, 4, 5, 6])
-tree3.pretty_print
+puts 'level order'
+p tree.level_order
+puts 'preorder'
+p tree.preorder
+puts 'postorder'
+p tree.postorder
+puts 'inorder'
+p tree.inorder
+
+puts 'inserting nodes...'
+tree.insert(101)
+tree.insert(102)
+tree.insert(103)
+tree.pretty_print
+puts 'balanced?'
+puts tree.balanced? # should be false
+
+puts 'rebalancing...'
+tree.rebalance
+tree.pretty_print
+puts 'balanced?'
+puts tree.balanced? # should be true
+
+puts 'level order'
+p tree.level_order
+puts 'preorder'
+p tree.preorder
+puts 'postorder'
+p tree.postorder
+puts 'inorder'
+p tree.inorder
